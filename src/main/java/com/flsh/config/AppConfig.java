39c34c4be337730aa0b2218152f28f1b0684f922
DaniHaiRun <danielson@hairun-technology.com>
2019-05-14 23:30:40 +0300

    Update config

---
+++ src/main/java/com/flsh/config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.flsh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Danielson Andriaritiana
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.flsh"
+})
+public class AppConfig {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/java/com/flsh/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,29 @@
+package com.flsh.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Danielson Andriaritiana
+ */
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class < ? > [] getRootConfigClasses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Class < ? > [] getServletConfigClasses() {
+        return new Class[] {
+            AppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {
+            "/"
+        };
+    }
+}
