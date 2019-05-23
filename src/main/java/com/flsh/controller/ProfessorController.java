45f135d05bcf0e4a9741c31386f8579cce891106
Rajaomalala <rajaomalalasendrampanjato@gmail.com>
2019-05-23 21:15:14 +0300

    professor view

--- src/main/java/com/flsh/controller/ProfessorController.java
+++ src/main/java/com/flsh/controller/ProfessorController.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.flsh.model.Login;
-import com.flsh.model.Professors;
+// import com.flsh.model.Login;
+// import com.flsh.model.Professors;
 import com.flsh.service.ProfessorsService;
 
 @Controller
@@ -21,23 +21,7 @@ public class ProfessorController {
 	  @RequestMapping(value = "/professors", method = RequestMethod.GET)
 	  public ModelAndView showLogin(HttpServletRequest request, HttpServletResponse response) {
 	    ModelAndView prof = new ModelAndView("professors");
-	    prof.addObject("professors", new Login());
-	    return prof;
-	  }
-
-	  @RequestMapping(value = "/loginProfessors", method = RequestMethod.POST)
-	  public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("login") Login login) {
-
-	    ModelAndView prof = null;
-	    Professors professor = professorsService.validateProfessor(login);
-	    if (null != professor) {
-	    	prof = new ModelAndView("welcome_prof");
-	    	prof.addObject("firstname", professor.getProfessorName());
-	    } else {
-		    prof = new ModelAndView("professors");
-		    prof.addObject("message_prof", "Username or Password is wrong!!");
-	    }
-	    
+	    prof.addObject("professors");
 	    return prof;
 	  }
 }
--- src/main/webapp/WEB-INF/views/professors.jsp
+++ src/main/webapp/WEB-INF/views/professors.jsp
@@ -4,35 +4,15 @@
 <html class="no-js" lang="en">
 <head>
 	<%@include file="common/_header.jsp" %>
-	<link rel="stylesheet" href="<c:url value="/resources/css/login.css" />">
 </head>
 <body>
 	
+    <%@include file="common/_sidebar.jsp" %>
     <!-- Start Welcome area -->
     <div class="all-content-wrapper">
-    	<div class="wrapper fadeInDown">
-			  <div id="formContent">
-				    <!-- Tabs Titles -->
-				
-				    <!-- Icon -->
-				    <div class="fadeIn first">
-						<img src="/scolarLMD/resources/img/profile/user.png" id="icon" alt="User Icon" />
-				    </div>
-				
-				    <!-- Login Form -->
-				    <form method = "POST" action = "loginProfessors">
-						<input type="text" id="login" class="fadeIn second" name="username" placeholder="login">
-						<input type="password" id="password" class="fadeIn third" name="password" placeholder="password">
-						<input type="submit" class="fadeIn fourth" value="Log In">
-				    </form>
-				
-				    <!-- Remind Passowrd -->
-				    <div id="formFooter">
-						<a class="underlineHover" href="#">Forgot Password?</a>
-				    </div>
-			
-			  </div>
-		</div>
+    	<%@include file="common/_menu.jsp" %>
+    	<%@include file="professor.jsp" %>
+    	<%@include file="common/_footer.jsp" %>
     </div>
 
 <%@include file="common/_script.jsp" %>
