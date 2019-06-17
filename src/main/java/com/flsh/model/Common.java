2ab4928c8204f696166fa46105c278c4f256323a
Rajaomalala <rajaomalalasendrampanjato@gmail.com>
2019-06-17 07:52:54 +0300

    some change after the stash

--- src/main/java/com/flsh/model/Common.java
+++ src/main/java/com/flsh/model/Common.java
@@ -1,5 +1,19 @@
 package com.flsh.model;
 
 public class Common {
+    private String name;
+    private String lastname;
     
+    public String getName() {
+        return name;	
+    }
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public String getLastName() {
+    	return lastname;
+    }
+    public void setLastName(String lastname) {
+    	this.lastname = lastname;
+    }
 }
