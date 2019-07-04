1c5087ad6d1ce13a342136358f74127c800520f4
Rajaomalala <rajaomalalasendrampanjato@gmail.com>
2019-07-04 20:12:33 +0300

    the config

--- src/main/java/com/flsh/utils/Config.java
+++ src/main/java/com/flsh/utils/Config.java
@@ -2,6 +2,6 @@ package com.flsh.utils;
 
 public class Config {
 	public static final String DB_USER = "sendra";
-	public static final String DB_PASSWORD = "sendra93";
+	public static final String DB_PASSWORD = "admin";
 	public static final String DB_URL = "localhost:3306/db_anglais?createDatabaseIfNotExist=true";
 }
