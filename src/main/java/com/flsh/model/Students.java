8b47b3cb89fb5f6948e295dc8a7809fe895a83fc
Rajaomalala <rajaomalalasendrampanjato@gmail.com>
2019-07-17 16:38:43 +0300

    creation of the common and extends to student first

--- src/main/java/com/flsh/model/Students.java
+++ src/main/java/com/flsh/model/Students.java
@@ -1,5 +1,6 @@
 package com.flsh.model;
 
-public class Students {
-
+public class Students extends Common{
+	// etd_id
+	// etd_nom 	etd_prenom 	etd_datenaissance 	etd_nationalité 	etd_adresse 	etd_email 	etd_dernieretab 	etd_nomconjoint 	etd_nompere 	etd_nommere 
 }
